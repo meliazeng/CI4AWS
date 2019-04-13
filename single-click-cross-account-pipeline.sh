8200077fd81c275c893b61b48a8dc926c82bb609
James Tsang <cgjames@hotmail.com>
2019-04-13 12:06:24 -0400

    remove portion for dev account

--- single-click-cross-account-pipeline.sh
+++ single-click-cross-account-pipeline.sh
@@ -3,10 +3,6 @@ echo -n "Enter ToolsAccount > "
 read ToolsAccount
 echo -n "Enter ToolsAccount ProfileName for AWS Cli operations> "
 read ToolsAccountProfile
-echo -n "Enter Dev Account > "
-read DevAccount
-echo -n "Enter DevAccount ProfileName for AWS Cli operations> "
-read DevAccountProfile
 echo -n "Enter Test Account > "
 read TestAccount
 echo -n "Enter TestAccount ProfileName for AWS Cli operations> "
@@ -16,15 +12,12 @@ read ProdAccount
 echo -n "Enter ProdAccount ProfileName for AWS Cli operations> "
 read ProdAccountProfile
 
-aws cloudformation deploy --stack-name pre-reqs --template-file ToolsAcct/pre-reqs.yaml --parameter-overrides DevAccount=$DevAccount TestAccount=$TestAccount ProductionAccount=$ProdAccount --profile $ToolsAccountProfile
+aws cloudformation deploy --stack-name pre-reqs --template-file ToolsAcct/pre-reqs.yaml --parameter-overrides TestAccount=$TestAccount ProductionAccount=$ProdAccount --profile $ToolsAccountProfile
 echo -n "Enter S3 Bucket created from above > "
 read S3Bucket
 
 echo -n "Enter CMK ARN created from above > "
 read CMKArn
-
-echo -n "Executing in DEV Account"
-aws cloudformation deploy --stack-name toolsacct-codepipeline-role --template-file DevAccount/toolsacct-codepipeline-codecommit.yaml --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ToolsAccount=$ToolsAccount CMKARN=$CMKArn --profile $DevAccountProfile
 
 echo -n "Executing in TEST Account"
 aws cloudformation deploy --stack-name toolsacct-codepipeline-cloudformation-role --template-file TestAccount/toolsacct-codepipeline-cloudformation-deployer.yaml --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ToolsAccount=$ToolsAccount CMKARN=$CMKArn  S3Bucket=$S3Bucket --profile $TestAccountProfile
@@ -34,7 +27,7 @@ aws cloudformation deploy --stack-name toolsacct-codepipeline-cloudformation-rol
 
 
 echo -n "Creating Pipeline in Tools Account"
-aws cloudformation deploy --stack-name sample-lambda-pipeline --template-file ToolsAcct/code-pipeline.yaml --parameter-overrides DevAccount=$DevAccount TestAccount=$TestAccount ProductionAccount=$ProdAccount CMKARN=$CMKArn S3Bucket=$S3Bucket --capabilities CAPABILITY_NAMED_IAM --profile $ToolsAccountProfile
+aws cloudformation deploy --stack-name sample-lambda-pipeline --template-file ToolsAcct/code-pipeline.yaml --parameter-overrides TestAccount=$TestAccount ProductionAccount=$ProdAccount CMKARN=$CMKArn S3Bucket=$S3Bucket --capabilities CAPABILITY_NAMED_IAM --profile $ToolsAccountProfile
 
 echo -n "Adding Permissions to the CMK"
 aws cloudformation deploy --stack-name pre-reqs --template-file ToolsAcct/pre-reqs.yaml --parameter-overrides CodeBuildCondition=true --profile $ToolsAccountProfile
